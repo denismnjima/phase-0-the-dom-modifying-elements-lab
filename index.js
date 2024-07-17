d82eb19e03deedd548a784af840690a00842ea7c
denismnjima <denismnjima@gmail.com>
2024-07-17 13:57:30 +0300

    solved question

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Seth Mwebi is the champion";
